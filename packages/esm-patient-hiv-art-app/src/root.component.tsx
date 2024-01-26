3d4a28f57006190354d6bdb6ff1ab4930dd1aeea
Michaelndula <michaelndula@gmail.com>
2024-01-26 13:43:30 +0300

    Adding home UI to homepage

---
+++ packages/esm-patient-hiv-art-app/src/root.component.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SWRConfig } from 'swr';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeDashboard from './home-dashboard/home-dashboard.component';
+
+const swrConfiguration = {
+  errorRetryCount: 3,
+};
+
+const RootComponent: React.FC = () => {
+  console.log('Root Component');
+  return (
+    <main>
+      <SWRConfig value={swrConfiguration}>
+        <HomeDashboard />
+      </SWRConfig>
+    </main>
+  );
+};
+
+export default RootComponent;
